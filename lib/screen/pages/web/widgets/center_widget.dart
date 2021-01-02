3fc272389bacd736554ceb159b10e6f5c297e200
nikeshh <nikeshhbaskaran01@gmail.com>
2021-01-02 18:54:06 +0530

    Center widget changes

--- lib/screen/pages/web/widgets/center_widget.dart
+++ lib/screen/pages/web/widgets/center_widget.dart
@@ -57,8 +57,8 @@ class _CenterWidgetState extends State<CenterWidget> {
                   SizedBox(
                     width: 10,
                   ),
-                  //_listOfPostWidget(),
-                  //_loadMoreButtonWidget(),
+                  _listOfPostWidget(),
+                  _loadMoreButtonWidget(),
                 ],
               )
         ],
@@ -415,6 +415,308 @@ class _CenterWidgetState extends State<CenterWidget> {
   }
 
   Widget _sortByWidget() {
-    return Container();
+    return Container(
+      child: Row(
+        children: [
+          _rowDivider(thickness: 0.80),
+          SizedBox(
+            width: 10,
+          ),
+          InkWell(
+            onTap: () {},
+            child: Row(
+              children: [
+                Text("Sort by:"),
+                Text(
+                  "Top",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Icon(Icons.arrow_drop_down),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _rowDivider({double thickness}) {
+    return Container(
+      width: 530,
+      height: thickness,
+      decoration: BoxDecoration(color: Colors.black38),
+    );
+  }
+
+  Widget _listOfPostWidget() {
+    return Container(
+      child: ListView.builder(
+        itemCount: _postData.length,
+        physics: ScrollPhysics(),
+        shrinkWrap: true,
+        itemBuilder: (BuildContext context, int index) {
+          return _singlePostItem(index);
+        },
+      ),
+    );
+  }
+
+  Widget _singlePostItem(int index) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black87, width: 0.50),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: [
+          _headerPost(index),
+          _descriptionImageWidget(index),
+        ],
+      ),
+    );
+  }
+
+  Widget _headerPost(int index) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Row(
+              children: [
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                  ),
+                  child: ClipRRect(
+                    child: Image.asset(_postData[index].profileUrl),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(_postData[index].name),
+                    Container(
+                      width: 530,
+                      child: Text(
+                        _postData[index].headline,
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black.withOpacity(0.4),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Icon(Icons.more_horiz),
+        ],
+      ),
+    );
+  }
+
+  Widget _descriptionImageWidget(int index) {
+      return Container(
+        width: 600,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Text(_postData[index].description),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              _postData[index].tags,
+              style: TextStyle(color: blueColor),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            _postData[index].isVideoPost == true
+              ? Container()
+                : Container(
+                  height: 500,
+                  width: 600,
+              child: Image.asset(
+                _postData[index].image,
+                fit: BoxFit.contain,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 25,
+                  height: 25,
+                  child: Image.asset("assets/icons/like_icon.png"),
+                ),
+                Container(
+                  width: 25,
+                  height: 25,
+                  child: Image.asset("assets/icons/celebrate_icon.png"),
+                ),
+                if (index == 1 || index == 4 || index == 6)
+                  Container(
+                    width: 25,
+                    height: 25,
+                    child: Image.asset("assets/icons/love_icon.png"),
+                  ),
+                SizedBox(
+                  width: 4,
+                ),
+                Text(_postData[index].likes),
+                Text(
+                  "⚫",
+                  style: TextStyle(fontSize: 10, color: Colors.blueGrey),
+                ),
+                Text(_postData[index].comments),
+              ],
+            ),
+            _rowButtonsLikeCommentWidget(),
+          ],
+        ),
+      );
+  }
+
+  Widget _rowButtonsLikeCommentWidget() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+      child: Row(
+        children: [
+          InkWell(
+            onTap: () {},
+            onHover: (hover) {
+              setState(() {
+                _rowButtonHover[0] = hover ? true : false;
+              });
+            },
+            child: _singleLikeCommentButtonWidget(
+              color: Colors.black,
+              iconImage: 'assets/icons/like_icon_white.png',
+              name: "Like",
+              isHover: _rowButtonHover[0],
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            onHover: (hover) {
+              setState(() {
+                _rowButtonHover[1] = hover ? true : false;
+              });
+            },
+            child: _singleLikeCommentButtonWidget(
+              color: Colors.black,
+              iconImage: 'assets/icons/comment_icon.png',
+              name: "Comment",
+              isHover: _rowButtonHover[1],
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            onHover: (hover) {
+              setState(() {
+                _rowButtonHover[2] = hover ? true : false;
+              });
+            },
+            child: _singleLikeCommentButtonWidget(
+              color: Colors.black,
+              iconImage: 'assets/icons/share_icon.png',
+              name: "Share",
+              isHover: _rowButtonHover[2],
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            onHover: (hover) {
+              setState(() {
+                _rowButtonHover[3] = hover ? true : false;
+              });
+            },
+            child: _singleLikeCommentButtonWidget(
+              color: Colors.black,
+              iconImage: 'assets/icons/sent_icon.png',
+              name: "Send",
+              isHover: _rowButtonHover[3],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _singleLikeCommentButtonWidget({
+    String name,
+    String iconImage,
+    Color color,
+    bool isHover
+  }) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 4, horizontal: 6),
+      decoration: BoxDecoration(
+        color: isHover ? Colors.black.withOpacity(0.1) : Colors.transparent,
+        borderRadius: BorderRadius.all(Radius.circular(4)),
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 25,
+            height: 25,
+            child: Image.asset(
+              iconImage,
+              color: color,
+            ),
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          Text(
+            name,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _loadMoreButtonWidget() {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          _isLoadMore = true;
+        });
+        Future.delayed(Duration(seconds: 4), () {
+          setState(() {
+            _isLoadMore = false;
+          });
+        });
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 10),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+        decoration: BoxDecoration(
+          color: Colors.white60,
+          border: Border.all(color: Colors.blueGrey, width: 1),
+          borderRadius: BorderRadius.all(Radius.circular(10))
+        ),
+        child: Text("Load More"),
+      ),
+    );
   }
 }
