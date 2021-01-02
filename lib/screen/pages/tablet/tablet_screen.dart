5465933db31a58e560c314acf04d5a547a2a72fe
nikeshh <nikeshhbaskaran01@gmail.com>
2021-01-03 01:25:12 +0530

    Added support for mobile and tablet

--- lib/screen/pages/tablet/tablet_screen.dart
+++ lib/screen/pages/tablet/tablet_screen.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:linkedinclone/screen/pages/tablet/nav_bar_tablet.dart';
+import 'package:linkedinclone/screen/pages/web/body_content.dart';
 
 class TabletScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            NavBarTablet(),
+            Expanded(child: BodyContent()),
+          ],
+        ),
+      ),
+    );
   }
 }
